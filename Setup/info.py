33d41c0a270bc829af3870f6d248f4391fb544ff
001parthdev-code <001.parth.dev@gmail.com>
2026-07-30 21:14:28 +0530

    D1

---
+++ Setup/info.py
@@ -0,0 +1,18 @@
+from zoneinfo import ZoneInfo
+from datetime import datetime
+
+class Info:
+    def __init__(self):
+        self.name = None
+        self.date_of_birth = None
+        self.language = None
+        self.local_timezone = None
+        self.datetime = None
+
+    def collect_info(self):
+        print("Welcome to SWAR!")
+        print("Let's get you set up\n")
+
+        self.name = 
+
+
