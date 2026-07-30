81d620e6d965bdfb7c2f2b5700ba1e41c3be30b0
001parthdev-code <001.parth.dev@gmail.com>
2026-07-31 00:33:42 +0530

    D2

--- Setup/info.py
+++ Setup/info.py
@@ -13,6 +13,20 @@ class Info:
         print("Welcome to SWAR!")
         print("Let's get you set up\n")
 
-        self.name = 
+        self.name = input("What should I call you?")
+        self.date_of_birth = input("When's yout birthday?")
+        self.language = input("What language do you speak?")
+
+        self.local_timezone =  datetime.now().astimezone().tzinfo
+        self.datetime =  datetime.now().astimezone()
+
+
+    def display_info(self):
+        print("------USER INFO------")
+        print(f"Name: {self.name}")
+        print(f"Date of Birth: {self.date_of_birth}")
+        print(f"Datetime: {self.datetime}")
+        print(f"Timezone: {self.local_timezone}")
+        print(f"Language: {self.language}")
 
 
---
+++ main.py
@@ -0,0 +1,10 @@
+from Setup.info import Info
+
+def main():
+    user = Info()
+    user.collect_info()
+    user.display_info()
+
+
+if __name__ == "__main__":
+    main()
