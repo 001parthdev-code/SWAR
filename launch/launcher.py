462e039babe20223300233493c746df102bd1c61
001parthdev-code <001.parth.dev@gmail.com>
2026-08-14 02:59:34 +0530

    Interactive layer

--- launch/launcher.py
+++ launch/launcher.py
@@ -1,5 +1,6 @@
 from setup.info import Profile
 from storage.storage import Storage
+from interaction.interact import Interaction
 
 
 class Launcher:
@@ -20,4 +21,11 @@ class Launcher:
 
         else:
 
+            
             print(f"Welcome back, {profile['name']}!")
+
+            interaction = Interaction(profile)
+
+            today = interaction.start()
+
+            print(f"\nGot it. You're working on: {today}")
